27f605134d71cb31046c9295e182c415420de042
Adam Cooper <adam@networkpie.co.uk>
2019-03-06 21:17:47 +0000

    Our basic server

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.use(require('express-naked-redirect')({
+  reverse: true,
+  status: 301
+}))
+app.use(express.static('public'))
+app.set('view engine', 'pug')
+
+app.get('/', function (req, res) {
+  res.status(200);
+  res.render('index');
+})
+
+app.listen(process.env.PORT, function () {
+  console.log('App listening on port ' + process.env.PORT + '!')
+})
